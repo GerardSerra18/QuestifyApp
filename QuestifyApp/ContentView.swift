0f88483a01294af6a217f0d13200cf88083820cd
GerardSerra18 <gerard.serra@estudiants.urv.cat>
2024-04-11 20:33:17 +0200

    Initial Commit

---
+++ QuestifyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuestifyApp
+//
+//  Created by Gerard Serra Rodríguez on 11/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world this is the questify APP!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
