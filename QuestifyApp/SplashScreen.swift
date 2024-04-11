5b5bd7e3db120a94116d9d2b81fd659dde327b6c
GerardSerra18 <gerard.serra@estudiants.urv.cat>
2024-04-11 21:16:37 +0200

    Splash Screen

---
+++ QuestifyApp/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  QuestifyApp
+//
+//  Created by Gerard Serra Rodríguez on 11/4/24.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
